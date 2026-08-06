7e26e2f8d1c3ff55da8be0d1b945c650bdf79a4f
animedotnet15-ops <animedotnet15@gmail.com>
2026-08-06 08:11:10 +0530

    Register API routes in health server

--- web/health.py
+++ web/health.py
@@ -2,6 +2,7 @@ from aiohttp import web
 
 import config.settings as config
 from core.logger import get_logger
+from web.api import register_api_routes
 
 LOGGER = get_logger("web.health")
 
@@ -14,6 +15,7 @@ async def start_health_server():
     app_ = web.Application()
     app_.router.add_get("/", health)
     app_.router.add_get("/health", health)
+    register_api_routes(app_)
     runner = web.AppRunner(app_)
     await runner.setup()
     site = web.TCPSite(runner, "0.0.0.0", config.PORT)
